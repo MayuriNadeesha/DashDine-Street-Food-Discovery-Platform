b84864f4ed4e2b77b9923aaf3286fc878cded34f
Uthpala Pathirana <uthpalanayanaramanipathirana@gmail>
2025-03-23 12:56:17 +0530

    Added user dashboard section

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom'; // Import useNavigate hook
 import './SignUp.css'; // You can style the page with this CSS file
 
